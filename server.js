4d1dad593c3520e04d7cd8b6a5a21e0f0687837a
Glitch (fcc-api-file-medata) <none>
2019-10-23 10:33:47 +0000

    🏂👲 Updated with Glitch

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-'use strict';
-
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var upload = multer();
 
 // require and use "multer"...
 
 var app = express();
+
+var fileName = "";
+var fileType = "";
+var fileSize = 0;
+
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +24,20 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile') ,function(req,res,next){
+
+  var fileName = req.file.originalname;
+  var fileType = req.file.mimetype;
+  var fileSize = req.file.size;
+
+res.send(`{"name":"${fileName}","type":"${fileType}","size":${fileSize}}`)
+})
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
